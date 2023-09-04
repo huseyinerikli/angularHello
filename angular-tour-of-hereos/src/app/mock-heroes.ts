3ac3988cd5c7431b71095f997140d100ff6e7d0b
Huseyin Erikli <herikli33@gmail.com>
2023-09-03 22:45:55 -0400

    last commit

--- angular-tour-of-hereos/src/app/mock-heroes.ts
+++ angular-tour-of-hereos/src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice', power: 'so nice', alterEgo: 'no alt ego'},
+  { id: 13, name: 'Bombasto', power: 'like bombas',},
+  { id: 14, name: 'Celeritas', power: 'celr' },
+  { id: 15, name: 'Magneta', power: 'so attractive' },
+  { id: 16, name: 'RubberMan', power: 'elastic' },
+  { id: 17, name: 'Dynama', power: 'strong' },
+  { id: 18, name: 'Dr. IQ', power: 'so smart' },
+  { id: 19, name: 'Magma' , power: 'sizzling'},
+  { id: 20, name: 'Tornado', power: 'so twisty' , alterEgo: 'so fast'}
 ];
